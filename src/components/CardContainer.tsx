9881ddbff427168bb5986e0389d7bf528ec85728
Kazeem Bankole <tunjibankole@gmail.com>
2023-01-18 16:10:14 +0100

    installed the autosuggest highlight package

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -3,6 +3,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import InputBase from '@mui/material/InputBase/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function CardContainer() {
   return (
@@ -13,6 +17,22 @@ export default function CardContainer() {
         <Typography align="left" variant="h6">
             Filter by keywords
         </Typography>
+        
+        <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+    >
+       <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="The most successful IT companies in 2020"
+        inputProps={{ 'aria-label': 'search input' }}
+      />
+     
+      
+    </Paper>
       </Container>
     </React.Fragment>
   );
