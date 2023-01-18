f52c3f35e4cfb0ab0e210e0c96bf0c1b0ee33e98
Kazeem Bankole <tunjibankole@gmail.com>
2023-01-18 16:01:54 +0100

    installed axios for fetching data from api

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 export default function CardContainer() {
   return (
@@ -9,6 +10,9 @@ export default function CardContainer() {
       <CssBaseline />
       <Container maxWidth="sm">
         <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+        <Typography align="left" variant="h6">
+            Filter by keywords
+        </Typography>
       </Container>
     </React.Fragment>
   );
