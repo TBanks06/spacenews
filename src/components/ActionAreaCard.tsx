971b631ebcef3a23b945af1d3b90787566575e62
Kazeem Bankole <tunjibankole@gmail.com>
2023-01-19 12:19:53 +0100

    created the cardcontainer details

--- src/components/ActionAreaCard.tsx
+++ src/components/ActionAreaCard.tsx
@@ -5,8 +5,21 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, IconButton } from '@mui/material';
 import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import styled from '@emotion/styled';
+import { width } from '@mui/system';
+
+const FlexDiv = styled.div({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'left',
+  gap: 5
+  // width: '100px',
+})
 
 export default function ActionAreaCard() {
+  
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
@@ -16,27 +29,36 @@ export default function ActionAreaCard() {
           image="https://images.pexels.com/photos/97050/pexels-photo-97050.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="green iguana"
         />
+
         <CardContent>
+
+    <FlexDiv>
+
           <IconButton type="button" sx={{ p: '5px' }} aria-label="calendar">
-            <CalendarTodayOutlinedIcon />
+            <CalendarTodayOutlinedIcon sx={{fontSize: "10px"}} />
         </IconButton>
 
           <Typography gutterBottom variant="caption" align="left" display="block" component="div">
           June 1st, 2021
 
           </Typography>
+    </FlexDiv>
           <Typography gutterBottom variant="h6" align="left" component="div">
           Healthcare updates
 
           </Typography>
-          <Typography variant="body1" align="left" gutterBottom>
+          <Typography variant="body2" align="left" gutterBottom>
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
           </Typography>
-          <Typography gutterBottom variant="h6" align="left" component="div">
-          Read more
+          <FlexDiv>
 
-          </Typography>
+              <Typography gutterBottom variant="h6" align="left" component="div">
+           
+                Read more
+              </Typography>
+              <ArrowForwardOutlinedIcon />
+          </FlexDiv>
         </CardContent>
       </CardActionArea>
     </Card>
--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -7,33 +7,97 @@ import Paper from '@mui/material/Paper';
 import IconButton from '@mui/material/IconButton';
 import InputBase from '@mui/material/InputBase/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
+import styled from '@emotion/styled';
+import Divider from '@mui/material/Divider';
+import ActionAreaCard from './ActionAreaCard';
+import { Grid } from '@mui/material';
+
+type ContainerProps ={
+    width: number;
+    height: number;
+}
+ const ContainerWrapper = styled.div({
+
+   width: 1000, 
+   height: 700,
+   border: 1,
+   borderColor: 'red'
+
+  
+  
+  
+ })
+
+   const ArticleContainer = styled.div({
+    width: '100%',
+    spacing: 2,
+
+
+   })
+ 
+
+
 
 export default function CardContainer() {
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
-        <Typography align="left" variant="h6">
+     
+      <ContainerWrapper >
+        <Box sx={{ bgcolor: '#cfe8fc' }} />
+        <Typography align="left" variant="h6" mb={3} p={3}>
             Filter by keywords
         </Typography>
         
         <Paper
       component="form"
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 } }
     >
        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
         <SearchIcon />
       </IconButton>
-      <InputBase
+      <InputBase 
         sx={{ ml: 1, flex: 1 }}
         placeholder="The most successful IT companies in 2020"
         inputProps={{ 'aria-label': 'search input' }}
       />
      
       
-    </Paper>
-      </Container>
+      </Paper>
+
+      <Typography align="left" variant="h6" mt={5} p={3}>
+            Results:
+        </Typography>
+        <Divider />
+
+    <ArticleContainer >
+    
+      <Box  sx={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' } }mb={3}>
+        <Grid container mx={1} p={3}>
+          <ActionAreaCard />
+          </Grid>
+        
+          <Grid container mx={1} p={3}>
+          <ActionAreaCard />
+          </Grid>
+
+          <Grid container mx={1} p={3}>
+          <ActionAreaCard />
+          </Grid>
+          <Grid container mx={1} p={3}>
+          <ActionAreaCard />
+          </Grid>
+          <Grid container mx={1} p={3}>
+          <ActionAreaCard />
+          </Grid>
+          <Grid container mx={1} p={3}>
+          <ActionAreaCard />
+          </Grid>
+
+      </Box>
+    
+      </ArticleContainer>
+          
+      </ContainerWrapper>
     </React.Fragment>
   );
 }
